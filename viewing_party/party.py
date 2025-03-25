706400d53b7a1c7951cabc628e3526a55578e73e
Yasiel <toyasiel@gmail.com>
2025-03-25 13:38:55 -0700

    wave two completed and passed tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,29 +1,40 @@
-# ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    if title and genre and rating:
-        return {
-            "title": title,
-            "genre": genre,
-            "rating": rating
-        }
-    else:
+
+
+def get_watched_avg_rating(user_data):
+    # gets the list of movies that have been watched. If empty, return 0.
+    watched = user_data["watched"]
+    if watched == []:
+        return 0.0
+    # sums the ratings of all the movies that have been watched. returns average rating.
+    total = 0
+    for movie in watched:
+        total += movie["rating"]
+
+    return total / len(watched)
+
+def get_most_watched_genre(user_data):
+    # If the user hasn't watched any movies, return None
+    if not user_data["watched"]:
         return None
-    
-def add_to_watched(user_data, movie):
-    user_data["watched"].append(movie)
-    return user_data
+
+    genre_count = {}
+
+    # Loop through watched movies and get genre of current movie
+    for movie in user_data["watched"]:
+        genre = movie["genre"] 
+        
+        if genre in genre_count:
+            genre_count[genre] += 1
+
+        else:
+            genre_count[genre] = 1
+
+    # return genre with highest count of movies
+    most_watched = max(genre_count, key=genre_count.get)
+    return most_watched
 
 
-
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
-
-
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
 
         
 # -----------------------------------------
@@ -35,5 +46,3 @@ def add_to_watched(user_data, movie):
 # -----------------------------------------
 
 
-def hello_fubction():
-    return "Hello"
