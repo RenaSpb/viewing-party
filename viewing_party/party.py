a88a14586ffab32743c69322ade63116b78d85d2
Renata <murzinarenata@gmail.com>
2025-03-25 13:48:23 -0700

    wave 1 1st function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,12 @@
+def create_movie(title, genre, rating):
+    if title and genre and rating:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+        }
+    return None
+
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
 
