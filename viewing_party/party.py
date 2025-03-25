18efe981f1fbd5f71516152effcf54b2eea238aa
Yasiel <toyasiel@gmail.com>
2025-03-25 13:45:54 -0700

    corrected order

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,21 @@
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
 
+    return user_data
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            break
+    
+    return user_data
 
 def get_watched_avg_rating(user_data):
     # gets the list of movies that have been watched. If empty, return 0.
@@ -17,7 +33,6 @@ def get_most_watched_genre(user_data):
     # If the user hasn't watched any movies, return None
     if not user_data["watched"]:
         return None
-<<<<<<< HEAD
 
     genre_count = {}
 
@@ -34,29 +49,10 @@ def get_most_watched_genre(user_data):
     # return genre with highest count of movies
     most_watched = max(genre_count, key=genre_count.get)
     return most_watched
-=======
-    
-def add_to_watched(user_data, movie):
-    user_data["watched"].append(movie)
 
-    return user_data
->>>>>>> 0edb5d50fe299586417af7e8d6047daca2bcdf59
-
-def add_to_watchlist(user_data, movie):
-    user_data["watchlist"].append(movie)
-
-    return user_data
-
-def watch_movie(user_data, title):
-    for movie in user_data["watchlist"]:
-        if movie["title"] == title:
-            user_data["watchlist"].remove(movie)
-            user_data["watched"].append(movie)
-            break
-    
-    return user_data
-
-
+# -----------------------------------------
+# ------------- WAVE 3--------------------
+# -----------------------------------------
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
