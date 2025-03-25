0edb5d50fe299586417af7e8d6047daca2bcdf59
Renata <murzinarenata@gmail.com>
2025-03-25 13:36:57 -0700

    wave 1 3-4 functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -12,8 +12,22 @@ def create_movie(title, genre, rating):
     
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
+
     return user_data
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            break
+    
+    return user_data
 
 
 # -----------------------------------------
